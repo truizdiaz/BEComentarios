f8f5510e338de89d1226078302e5bed1af9b23ba
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-03 10:18:02 -0300

    Init

---
+++ Controllers/ComentarioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BEComentarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : ControllerBase
+    {
+        // GET: api/<ComentarioController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Angular", "React" };
+        }
+
+        // GET api/<ComentarioController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ComentarioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ComentarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ComentarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
