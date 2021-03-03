10f0c28e2b09414a24be0bc56afd37b9063e34c1
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2021-03-03 10:29:17 -0300

    Instalacion de paquetes

---
+++ Models/Comentario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Creador { get; set; }
+        [Required]
+        public string Texto { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
